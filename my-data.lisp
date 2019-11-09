9dcaeb5fe8a30464d13057db4c26264805fa8ead
Ian Johnson <ianprime0509@gmail.com>
2019-11-09 13:05:33 -0500

    Add personal resume data

---
+++ my-data.lisp
@@ -0,0 +1,56 @@
+((:basics
+  (:name "Ian Johnson")
+  (:address "2925 Rensselaer Court, Vienna, VA 22181")
+  (:phone "7038198495")
+  (:email "IanTimothyJohnson@gmail.com")
+  (:summary "I am a quick and versatile learner, adaptable to a variety of challenging situations and tasks. I have a strong background in mathematics, as well as experience in computer programming."))
+ (:education
+  ((:school "University of Virginia")
+   (:graduated (2017 5))
+   (:degree "B.A. in Mathematics (advanced track) and Physics")
+   (:gpa "3.96")
+   (:awards ("Echols Scholar"
+             "Edwin E. Floyd Prize in Mathematics (2017)"
+             "Intermediate Honors (2016)"
+             "Dean's List (Spring 2015, Spring 2016, Fall 2016)"))))
+ (:experience
+  ((:title "Volunteer Tutor")
+   (:dates ((2017 9)))
+   (:organization "FACETS")
+   (:location "Centreville, VA")
+   (:experiences ("Teach computer, math and English skills to adults and children in a low-income community."
+                  "Provide proactive technical support to community center.")))
+  ((:title "Undergraduate Physics Researcher")
+   (:dates ((2015 4) . (2017 9)))
+   (:organization "UVa Solid Polarized Target Group")
+   (:location "Charlottesville, VA")
+   (:experiences ("Develop simulations and mathematical models from systems of differential equations and real data."
+                  "Design and implement software to assist in lab tasks.")))
+  ((:title "Undergraduate Mathematics Researcher")
+   (:dates ((2016 5) . (2017 7)))
+   (:organization "University of Virginia")
+   (:location "Charlottesville, VA")
+   (:experiences ("Apply individual study of new material to research efforts."
+                  "Collaborate with research adviser to investigate novel concepts.")))
+  ((:title "Vice President and Secretary")
+   (:dates ((2015 5) . (2017 5)))
+   (:organization "Kinetic Sound")
+   (:location "Charlottesville, VA")
+   (:experiences ("Co-founded successful student organization."
+                  "Coordinate logistics of large-scale events with over one hundred attendees."))))
+ (:skills
+  ((:skill "Computer Proficiency")
+   (:details ("Experienced with Windows and GNU/Linux systems (day-to-day usage and basic administration)"
+              "Proficient in Microsoft Office (Word, Powerpoint, Excel) and equivalent products")))
+  ((:skill "Programming")
+   (:details ("Languages: C, JavaScript, Rust, LabVIEW, C++"
+              "Web tools: HTML, CSS, JSON"
+              "Development tools: Git, make, NPM/Yarn, GCC, POSIX shell scripting"
+              "GitHub: https://github.com/ianprime0509"))))
+ (:publications
+  ((:title "Chain Posets")
+   (:url "https://arxiv.org/abs/1802.05813"))
+  ((:title "Automated Microwave Frequency Control in Dynamic Nuclear Polarization Experiments")
+   (:url "http://meetings.aps.org/link/BAPS.2016.APR.K10.2"))
+  ((:title "Spin nilHecke algebras of classical type")
+   (:url "https://arxiv.org/abs/1706.06240"))))
